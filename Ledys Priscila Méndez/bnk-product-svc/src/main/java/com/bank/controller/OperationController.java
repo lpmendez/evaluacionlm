e5979fe69027ea10bb03dbcb45b4ee9d278ab30d
Priscila Méndez <priscymendez_26@hotmail.com>
2019-05-11 19:26:33 -0600

    item 6 y 7

--- Ledys Priscila Méndez/bnk-product-svc/src/main/java/com/bank/controller/OperationController.java
+++ Ledys Priscila Méndez/bnk-product-svc/src/main/java/com/bank/controller/OperationController.java
@@ -24,4 +24,50 @@ public class OperationController {
 		req.setUser(user);
 		return process.ownTransfer(req);
 	}
+
+	//pago a tarjeta de credito propia
+	@PostMapping("${config.endpoints.payment.own-card}")
+	public Transaction ownPayCard(
+			@RequestHeader("user") String user,
+			@RequestBody OperationReq req) {
+		req.setUser(user);
+		return process.cardPayment(req);
+	}
+
+	//pago a prestamo propia
+	@PostMapping("${config.endpoints.payment.own-loan}")
+	public Transaction ownPayLoan(
+			@RequestHeader("user") String user,
+			@RequestBody OperationReq req) {
+		req.setUser(user);
+		return process.loanPayment(req);
+	}
+	
+	//transferencia bancaria a terceros
+	@PostMapping("${config.endpoints.transfer.third}")
+	public Transaction thirdTransfer(
+			@RequestHeader("user") String user,
+			@RequestBody OperationReq req) {
+		req.setUser(user);
+		return process.thirdTransfer(req);
+	}
+
+	//pago a tarjeta de credito a terceros
+	@PostMapping("${config.endpoints.payment.third-card}")
+	public Transaction thirdPayCard(
+			@RequestHeader("user") String user,
+			@RequestBody OperationReq req) {
+		req.setUser(user);
+		return process.thirdCardPay(req);
+	}
+
+	//pago a prestamo a terceros
+	@PostMapping("${config.endpoints.payment.third-loan}")
+	public Transaction thirdLoanPay(
+			@RequestHeader("user") String user,
+			@RequestBody OperationReq req) {
+		req.setUser(user);
+		return process.thirdLoanPay(req);
+	}
+	
 }
--- Ledys Priscila Méndez/bnk-product-svc/src/main/java/com/bank/process/operation/OperationProcess.java
+++ Ledys Priscila Méndez/bnk-product-svc/src/main/java/com/bank/process/operation/OperationProcess.java
@@ -39,7 +39,6 @@ public class OperationProcess implements IOperationProcess {
 	
 	@Override
 	public Transaction ownTransfer(OperationReq req) {
-		// validar que las cuentas pertenezcan al cliente y que existan por supuesto
 		try {
 			// validar que las cuentas pertenezcan al cliente y que existan por supuesto y sean de tipo cuenta
 			AccountDetail from =  acc.retrieveByUsrAndId(req.getUser(), req.getFrom());
@@ -104,14 +103,130 @@ public class OperationProcess implements IOperationProcess {
 
 	@Override
 	public Transaction cardPayment(OperationReq req) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			// validar que las cuentas pertenezcan al cliente
+			AccountDetail from =  acc.retrieveByUsrAndId(req.getUser(), req.getFrom());
+			CardDetail to =  card.retrieveByUsrAndId(req.getUser(), req.getTo());
+			if(from == null || to == null) {
+				//devolver codigo 404
+				throw new ApplicationException(HttpStatus.valueOf(Integer.parseInt(ResponseCode.INVALID)),
+						ResponseCode.INVALID, ResponseMsg.INVALID);
+			}
+			//ahora validar que tenga el saldo suficiente.. para la transacción.
+			if(from.getBalance() < req.getAmount()) {
+				throw new ApplicationException(HttpStatus.valueOf(Integer.parseInt(ResponseCode.NO_BALANCE)),
+						ResponseCode.NO_BALANCE, ResponseMsg.NO_BALANCE);
+			}
+			//realizar descuentos. 
+			// Se resta de cuenta from
+			if(!acc.operation(req.getFrom(), req.getAmount()  * -1)) {
+				throw new ApplicationException(HttpStatus.valueOf(Integer.parseInt(ResponseCode.GENERAL)),
+						ResponseCode.GENERAL, "Operation error in FROM account");
+			}
+			// y sumar el available de la tarjeta..
+			if(!card.operation(req.getTo(), req.getAmount())) {
+				throw new ApplicationException(HttpStatus.valueOf(Integer.parseInt(ResponseCode.GENERAL)),
+						ResponseCode.GENERAL, "Operation error in CARD account");
+			}
+			//guardar las transacciones..
+
+			Transaction input = new Transaction();
+			input.setAccId(req.getTo());
+			input.setAmount(req.getAmount());
+			input.setDescription("Abono a pago de tarjeta de "+req.getFrom()+". "+req.getDescription());
+			input.setUsrCode(req.getUser());
+			if(tran.save(input) == null) {
+				throw new ApplicationException(HttpStatus.valueOf(Integer.parseInt(ResponseCode.GENERAL)),
+						ResponseCode.GENERAL, ResponseMsg.GENERAL);
+			}
+			
+			input = new Transaction();
+			input.setAccId(req.getFrom());
+			input.setAmount(req.getAmount() * -1);
+			input.setDescription("Pago de tarjeta No."+req.getTo()+". "+req.getDescription());
+			input.setUsrCode(req.getUser());
+			Transaction response = tran.save(input);
+
+			if(response == null) {
+				throw new ApplicationException(HttpStatus.valueOf(Integer.parseInt(ResponseCode.GENERAL)),
+						ResponseCode.GENERAL, ResponseMsg.GENERAL);
+			}
+			
+			return response;
+			
+		}
+		catch(ApplicationException ex) {
+			throw ex;
+		}
+		catch(Exception ex) {
+			log.error("Error in payment credit card. Error: {}. User: {}", ex.getMessage(), req.getUser(), ex);
+		}
+		throw new ApplicationException(HttpStatus.valueOf(Integer.parseInt(ResponseCode.GENERAL)),
+				ResponseCode.GENERAL, ResponseMsg.GENERAL);
 	}
 
 	@Override
 	public Transaction loanPayment(OperationReq req) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			// validar que las cuentas pertenezcan al cliente
+			AccountDetail from =  acc.retrieveByUsrAndId(req.getUser(), req.getFrom());
+			LoanDetail to =  loan.retrieveByUsrAndId(req.getUser(), req.getTo());
+			if(from == null || to == null) {
+				//devolver codigo 404
+				throw new ApplicationException(HttpStatus.valueOf(Integer.parseInt(ResponseCode.INVALID)),
+						ResponseCode.INVALID, ResponseMsg.INVALID);
+			}
+			//ahora validar que tenga el saldo suficiente.. para la transacción.
+			if(from.getBalance() < req.getAmount()) {
+				throw new ApplicationException(HttpStatus.valueOf(Integer.parseInt(ResponseCode.NO_BALANCE)),
+						ResponseCode.NO_BALANCE, ResponseMsg.NO_BALANCE);
+			}
+			//realizar descuentos. 
+			// Se resta de cuenta from
+			if(!acc.operation(req.getFrom(), req.getAmount()  * -1)) {
+				throw new ApplicationException(HttpStatus.valueOf(Integer.parseInt(ResponseCode.GENERAL)),
+						ResponseCode.GENERAL, "Operation error in FROM account");
+			}
+			// y restar el debt del prestamo..
+			if(!loan.operation(req.getTo(), req.getAmount() * -1)) {
+				throw new ApplicationException(HttpStatus.valueOf(Integer.parseInt(ResponseCode.GENERAL)),
+						ResponseCode.GENERAL, "Operation error in LOAN account");
+			}
+			//guardar las transacciones..
+
+			Transaction input = new Transaction();
+			input.setAccId(req.getTo());
+			input.setAmount(req.getAmount());
+			input.setDescription("Abono a pago de préstamo de "+req.getFrom()+". "+req.getDescription());
+			input.setUsrCode(req.getUser());
+			if(tran.save(input) == null) {
+				throw new ApplicationException(HttpStatus.valueOf(Integer.parseInt(ResponseCode.GENERAL)),
+						ResponseCode.GENERAL, ResponseMsg.GENERAL);
+			}
+			
+			input = new Transaction();
+			input.setAccId(req.getFrom());
+			input.setAmount(req.getAmount() * -1);
+			input.setDescription("Pago de préstamo No."+req.getTo()+". "+req.getDescription());
+			input.setUsrCode(req.getUser());
+			Transaction response = tran.save(input);
+
+			if(response == null) {
+				throw new ApplicationException(HttpStatus.valueOf(Integer.parseInt(ResponseCode.GENERAL)),
+						ResponseCode.GENERAL, ResponseMsg.GENERAL);
+			}
+			
+			return response;
+			
+		}
+		catch(ApplicationException ex) {
+			throw ex;
+		}
+		catch(Exception ex) {
+			log.error("Error in payment credit card. Error: {}. User: {}", ex.getMessage(), req.getUser(), ex);
+		}
+		throw new ApplicationException(HttpStatus.valueOf(Integer.parseInt(ResponseCode.GENERAL)),
+				ResponseCode.GENERAL, ResponseMsg.GENERAL);
 	}
 
 	@Override
